302584f469375259f6ebe066b2940bf226ca750c
AVSheard <a_sheard@yahoo.co.uk>
2021-02-13 11:49:29 +0000

    filled in users migration functions

--- db/migrations/20210212152153_users.js
+++ db/migrations/20210212152153_users.js
@@ -1,8 +1,12 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+	return knex.schema.createTable("users", (userTable) => {
+		userTable.string("username").primary().unique();
+		userTable.string("email").notNullable();
+		userTable.string("password").notNullable();
+		userTable.string("avatar_url");
+	});
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTable("users");
 };
