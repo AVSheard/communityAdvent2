3888e210a39db35e0a06142de537135ffb98a529
AVSheard <a_sheard@yahoo.co.uk>
2021-02-13 11:48:08 +0000

    removed name from data entries

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,19 +1,16 @@
 module.exports = [
 	{
 		username: "a_sheard",
-		name: "Anthony Sheard",
 		email: "a_sheard@yahoo.co.uk",
 		password: "password1",
 	},
 	{
 		username: "avsheard",
-		name: "Anthony Vincent Sheard",
 		email: "avsheard@gmail.com",
 		password: "password2",
 	},
 	{
 		username: "jcsheard",
-		name: "Julia Sheard",
 		email: "jcsheard@yahoo.co.uk",
 		password: "password3",
 	},
