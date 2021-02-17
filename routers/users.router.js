661ecb574b5bc4eab42f3319db16fe0fc8a29b79
AVSheard <a_sheard@yahoo.co.uk>
2021-02-17 10:57:28 +0000

    made test for invalid methods on /api/users

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,4 +1,5 @@
 const { requestUsers } = require("../controllers/users.controllers");
+const { invalidMethod } = require("../errors/errors");
 
 usersRouter = require("express").Router();
 
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -39,5 +39,17 @@ describe("/api", () => {
 					});
 				});
 		});
+		it("405 for invalid methods on /api/users", () => {
+			const invalidMethods = ["patch", "put", "delete", "post"];
+			const methodPromises = invalidMethods.map((method) => {
+				return request(app)
+					[method]("/api")
+					.expect(405)
+					.then(({ body: { msg } }) => {
+						expect(msg).to.equal("method not allowed");
+					});
+			});
+			return Promise.all(methodPromises);
+		});
 	});
 });
