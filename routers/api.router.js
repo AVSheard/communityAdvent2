9a6d18c609f095be2d8d19ed482086d5b8ca0980
AVSheard <a_sheard@yahoo.co.uk>
2021-02-17 09:50:52 +0000

    added the path on routers for GET - /api

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,7 +1,11 @@
+const { request } = require("express");
 const usersRouter = require("./users.router");
+const { requestJSON } = require("../controllers/api.controllers");
 
 apiRouter = require("express").Router();
 
 apiRouter.use("/users", usersRouter);
+
+apiRouter.route("/").get(requestJSON);
 
 module.exports = apiRouter;
