25acbbe596ea338a8dc53a3ee73d61a2f720f91f
AVSheard <a_sheard@yahoo.co.uk>
2021-02-17 10:17:03 +0000

    added invalid error handeling to api riuter

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,11 +1,12 @@
 const { request } = require("express");
 const usersRouter = require("./users.router");
+const { invalidMethod } = require("../errors/errors");
 const { requestJSON } = require("../controllers/api.controllers");
 
 apiRouter = require("express").Router();
 
 apiRouter.use("/users", usersRouter);
 
-apiRouter.route("/").get(requestJSON);
+apiRouter.route("/").get(requestJSON).all(invalidMethod);
 
 module.exports = apiRouter;
