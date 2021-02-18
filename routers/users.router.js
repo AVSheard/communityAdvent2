381edd2003d5bfdd916483e9bccf8e4f371c16df
AVSheard <a_sheard@yahoo.co.uk>
2021-02-18 16:47:52 +0000

    added the path GET - /api/users/:username to user router

--- routers/users.router.js
+++ routers/users.router.js
@@ -3,6 +3,8 @@ const { invalidMethod } = require("../errors/errors");
 
 usersRouter = require("express").Router();
 
+usersRouter.route("/:username").get(requestUser).all(invalidMethod);
+
 usersRouter.route("/").get(requestUsers).all(invalidMethod);
 
 module.exports = usersRouter;
