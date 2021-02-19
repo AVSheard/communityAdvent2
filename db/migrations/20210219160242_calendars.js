150809c4d2cffae9721695580ba7e7959f55968f
AVSheard <a_sheard@yahoo.co.uk>
2021-02-19 16:19:30 +0000

    filled in calendars migration

--- db/migrations/20210219160242_calendars.js
+++ db/migrations/20210219160242_calendars.js
@@ -1,8 +1,15 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+	return knex.schema.createTable("calendars", (calendarTable) => {
+		calendarTable.increments("calendar_id").primary();
+		calendarTable.string("name").notNullable();
+		calendarTable.string("centreName").notNullable();
+		calendarTable.float("centreLocLat").notNullable();
+		calendarTable.float("centreLocLong").notNullable();
+		calendarTable.string("picture_url").notNullable();
+		calendarTable.string("admin").references("users.username").notNullable();
+	});
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTable("calendars");
 };
