5cdf643b59549ade1b15953c7e4d95a16fe4f0bc
AVSheard <a_sheard@yahoo.co.uk>
2021-02-19 16:44:33 +0000

    made calendar development data

---
+++ db/data/development-data/calendars.js
@@ -0,0 +1,23 @@
+module.exports = [
+	{
+		name: "Tunstead Advent",
+		centreName: "Prospecton",
+		centreLocLong: 53.53861,
+		centreLocLat: -1.996911,
+		admin: "a_sheard",
+	},
+	{
+		name: "Greenfield Advent",
+		centreName: "St Mary's Church",
+		centreLocLong: 53.5330656,
+		centreLocLat: -1.9983334,
+		admin: "avsheard",
+	},
+	{
+		name: "Uppermill Advent",
+		centreName: "Uppermill Methodist Church",
+		centreLocLong: 53.5488627,
+		centreLocLat: -2.0042356,
+		admin: "avsheard",
+	},
+];
