b51724907b6d13b1ba7c10166cb1cb2d6cdf0b13
AVSheard <a_sheard@yahoo.co.uk>
2021-02-22 11:20:56 +0000

    added calendars router to api router

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,11 +1,14 @@
 const { request } = require("express");
 const usersRouter = require("./users.router");
+const calendarsRouter = require("./calendars.router");
 const { invalidMethod } = require("../errors/errors");
 const { requestJSON } = require("../controllers/api.controllers");
 
 apiRouter = require("express").Router();
 
 apiRouter.use("/users", usersRouter);
+
+apiRouter.use("/calendars", calendarsRouter);
 
 apiRouter.route("/").get(requestJSON).all(invalidMethod);
 
