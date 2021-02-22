d3d8fdc9871d8b2be852fa8851edbec9a2bba278
AVSheard <a_sheard@yahoo.co.uk>
2021-02-22 14:03:11 +0000

    error handled for invalid method on thr route /api/calendars

--- routers/calendars.router.js
+++ routers/calendars.router.js
@@ -1,7 +1,8 @@
 const { requestCalendars } = require("../controllers/calendars.controllers");
+const { invalidMethod } = require("../errors/errors");
 
 calendarsRouter = require("express").Router();
 
-calendarsRouter.route("/").get(requestCalendars);
+calendarsRouter.route("/").get(requestCalendars).all(invalidMethod);
 
 module.exports = calendarsRouter;
