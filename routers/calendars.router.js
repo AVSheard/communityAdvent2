3c2b4f56d4122019a24efb24007b0b0bb0ce07bc
AVSheard <a_sheard@yahoo.co.uk>
2021-02-22 14:25:46 +0000

    addedthe route GET - /api/calendars/calendar_id

--- routers/calendars.router.js
+++ routers/calendars.router.js
@@ -3,6 +3,8 @@ const { invalidMethod } = require("../errors/errors");
 
 calendarsRouter = require("express").Router();
 
+calendarsRouter.route("/calendar_id").get(requestCalendar);
+
 calendarsRouter.route("/").get(requestCalendars).all(invalidMethod);
 
 module.exports = calendarsRouter;
