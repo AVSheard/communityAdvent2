f09c48bbde08ef9be270a0923fab71a5213d8ca6
AVSheard <a_sheard@yahoo.co.uk>
2021-02-23 16:40:05 +0000

    corrected houses test data

--- db/data/test-data/houses.js
+++ db/data/test-data/houses.js
@@ -4,20 +4,20 @@ module.exports = [
 		day: 1,
 		houseLongLoc: 53.53861,
 		houseLatLoc: -1.996911,
-		calendarID: 1,
+		calendar_id: 1,
 	},
 	{
 		houseName: "Prospecton Farm, Tunstead Lane",
 		day: 24,
 		houseLongLoc: 53.538317,
 		houseLatLoc: -1.997753,
-		calendarID: 1,
+		calendar_id: 1,
 	},
 	{
 		houseName: "Boarshurst Centre",
 		day: 2,
 		houseLongLoc: 53.5366369,
 		houseLatLoc: -2.0009422,
-		calendarID: 2,
+		calendar_id: 2,
 	},
 ];
