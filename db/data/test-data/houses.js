abf1dc46c7d1a0916606817b5a3afd10d6036b1e
AVSheard <a_sheard@yahoo.co.uk>
2021-02-23 09:35:34 +0000

    made test data for houses

---
+++ db/data/test-data/houses.js
@@ -0,0 +1,23 @@
+module.exports = [
+	{
+		name: "Prospecton, Tunstead Lane",
+		day: 1,
+		longLocation: 53.53861,
+		latLocation: -1.996911,
+		calendar: "Tunstead Advent",
+	},
+	{
+		name: "Prospecton Farm, Tunstead Lane",
+		day: 24,
+		longLocation: 53.538317,
+		latLocation: -1.997753,
+		calendar: "Tunstead Advent",
+	},
+	{
+		name: "Boarshurst Centre",
+		day: 2,
+		longLocation: 53.5366369,
+		latLocation: -2.0009422,
+		calendar: "Greenfield Advent",
+	},
+];
