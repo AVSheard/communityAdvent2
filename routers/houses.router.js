26d53a27f55f0cea028fd8f19b7b63b6713ddeac
AVSheard <a_sheard@yahoo.co.uk>
2021-02-24 09:42:01 +0000

    error handled for invalid method on the path /api/houses

--- routers/houses.router.js
+++ routers/houses.router.js
@@ -1,7 +1,8 @@
 const { requestHouses } = require("../controllers/houses.controllers");
+const { invalidMethod } = require("../errors/errors");
 
 housesRouter = require("express").Router();
 
-housesRouter.route("/").get(requestHouses);
+housesRouter.route("/").get(requestHouses).all(invalidMethod);
 
 module.exports = housesRouter;
