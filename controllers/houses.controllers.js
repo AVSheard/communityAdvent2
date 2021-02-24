ac7a3f690d66bd285c4d80638eca8c14a58367b1
AVSheard <a_sheard@yahoo.co.uk>
2021-02-24 15:13:52 +0000

    error handled calendar id query that does not exist

--- controllers/houses.controllers.js
+++ controllers/houses.controllers.js
@@ -1,9 +1,13 @@
 const { getHouses } = require("../models/houses.models");
 
 const requestHouses = (request, response, next) => {
-	getHouses(request.query.calendar_id).then((houses) => {
-		response.status(200).send({ houses });
-	});
+	getHouses(request.query.calendar_id)
+		.then((houses) => {
+			response.status(200).send({ houses });
+		})
+		.catch((err) => {
+			next(err);
+		});
 };
 
 module.exports = { requestHouses };
--- models/houses.models.js
+++ models/houses.models.js
@@ -1,15 +1,25 @@
 const connection = require("../db/connection");
+const { doesItemExist } = require("./middleware.models");
 
 const getHouses = (reqCalID) => {
-	return connection
-		.select("*")
-		.from("houses")
-		.returning("*")
-		.modify((query1) => {
-			if (reqCalID) {
-				query1.where("houses.calendar_id", reqCalID);
-			}
-		});
+	let calIdReal = true;
+
+	if (reqCalID) calIdReal = doesItemExist(reqCalID, "calendar_id", "calendars");
+	return Promise.all([calIdReal]).then((booleans) => {
+		if (reqCalID && !booleans[0]) {
+			return Promise.reject({ status: 404, msg: "Calendar_id does not exist" });
+		}
+
+		return connection
+			.select("*")
+			.from("houses")
+			.returning("*")
+			.modify((query1) => {
+				if (reqCalID) {
+					query1.where("houses.calendar_id", reqCalID);
+				}
+			});
+	});
 };
 
 module.exports = { getHouses };
