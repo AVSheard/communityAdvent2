17682d937c071d0fdd1971c40e6be2fa76e30cd7
AVSheard <a_sheard@yahoo.co.uk>
2021-05-20 15:38:07 +0100

    changed controller test

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,10 +1,11 @@
 const { getUsers, getUser } = require("../models/users.models");
 
 const requestUsers = (request, response, next) => {
+	response.status(200).send({ user: "anthony" });
 	console.log("controller");
-	getUsers().then((users) => {
-		response.status(200).send({ users });
-	});
+	// getUsers().then((users) => {
+	// 	response.status(200).send({ users });
+	// });
 };
 
 const requestUser = (request, response, next) => {
