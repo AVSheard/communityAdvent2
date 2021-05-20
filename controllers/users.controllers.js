7774cadd34a6ac2b09aa990fe15855b3fda6872a
AVSheard <a_sheard@yahoo.co.uk>
2021-05-20 16:12:50 +0100

    removed response test from controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -2,7 +2,6 @@ const { getUsers, getUser } = require("../models/users.models");
 
 const requestUsers = (request, response, next) => {
 	console.log("controller");
-	response.status(200).send({ users: "anthony" });
 
 	getUsers().then((users) => {
 		console.log(users);
