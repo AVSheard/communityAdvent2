96f540422e7c62a1cc710c8fc874369f7f03e5a4
AVSheard <a_sheard@yahoo.co.uk>
2021-05-25 15:19:07 +0100

    reverted model to origional

--- models/users.models.js
+++ models/users.models.js
@@ -1,34 +1,33 @@
-// const connection = require("../db/connection");
+const connection = require("../db/connection");
 
 const getUsers = () => {
 	console.log("model");
-	return [{ user: "anthony" }, { user: "vincent" }];
-	// return connection
-	// 	.select("*")
-	// 	.from("users")
-	// 	.returning("*")
-	// 	.then((users) => {
-	// 		return users.map((user) => {
-	// 			delete user["password"];
-	// 			return user;
-	// 		});
-	// 	});
+
+	return connection
+		.select("*")
+		.from("users")
+		.returning("*")
+		.then((users) => {
+			return users.map((user) => {
+				delete user["password"];
+				return user;
+			});
+		});
 };
 
 const getUser = (userUsername) => {
 	console.log("wrong model");
-	return { users: "anthony" };
-	// return connection
-	// 	.select("*")
-	// 	.from("users")
-	// 	.where({ username: userUsername })
-	// 	.returning("*")
-	// 	.then((user) => {
-	// 		if (user.length === 0)
-	// 			return Promise.reject({ status: 404, msg: "Username does not exist" });
-	// 		delete user[0]["password"];
-	// 		return user[0];
-	// 	});
+	return connection
+		.select("*")
+		.from("users")
+		.where({ username: userUsername })
+		.returning("*")
+		.then((user) => {
+			if (user.length === 0)
+				return Promise.reject({ status: 404, msg: "Username does not exist" });
+			delete user[0]["password"];
+			return user[0];
+		});
 };
 
 module.exports = { getUsers, getUser };
